9a27df7ccf651f86d6d75355d17a3f62d2247c5a
Rob Hill <robert.hill0122@gmail.com>
2014-08-29 10:01:41 -0600

    add tests for form validations

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -33,4 +33,34 @@ feature 'User creation and login' do
     sign_in
   end
 
+  scenario "username must be unique" do
+    sign_in
+    click_on "LOGOUT"
+    click_on "Join"
+    expect(page).to have_content("Register")
+    fill_in "Username", with: "Rob"
+    fill_in "Password", with: "password"
+    fill_in "First name", with: "Rob"
+    fill_in "First name", with: "Rob"
+    fill_in "Last name", with: "Hill"
+    fill_in "Bio", with: "Just a simple fellow who likes to Rant"
+    click_on "Register"
+    expect(page).to have_content "Username has already been taken"
+  end
+
+  scenario "user must enter username.  password must also be at least 8 characters" do
+    visit '/'
+    click_on "Join"
+    expect(page).to have_content("Register")
+    fill_in "Username", with: ""
+    fill_in "Password", with: "pass"
+    fill_in "First name", with: "Rob"
+    fill_in "First name", with: "Rob"
+    fill_in "Last name", with: "Hill"
+    fill_in "Bio", with: "Just a simple fellow who likes to Rant"
+    click_on "Register"
+    expect(page).to have_content "Username can't be blank"
+    expect(page).to have_content "Password is too short"
+  end
+
 end
