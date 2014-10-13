34bd585f624090ea1cbbc316c1d8cedd409cfd7a
Rob Hill <robert.hill0122@gmail.com>
2014-10-13 16:35:20 -0600

    refactored user#show by creating #sorted_rants

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ActiveRecord::Base
     users = User.where("username LIKE :query OR last_name LIKE :query OR first_name LIKE :query", query: "%#{query}%")
     Rant.where(user_id: users.each {|user| user.id})
   end
+
+  def sorted_rants
+    self.rants.sort_by { |r| FavoritedRant.where(rant_id: r.id).count}.reverse
+  end
 end
