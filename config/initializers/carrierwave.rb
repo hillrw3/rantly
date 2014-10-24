e77f521ae8a33107afae77f7c730b85292f92432
Rob Hill <robert.hill0122@gmail.com>
2014-10-24 15:51:50 -0600

    removed unnecessary migrations for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'] || Rails.configuration.s3_access_key_id,                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'] || Rails.configuration.s3_secret_access_key,                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                        # required
   }
   config.fog_directory  = 'rantly-dev'                          # required
   config.fog_public     = true                                        # optional, defaults to true
--- db/migrate/20141017222349_add_admin_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :admins, :boolean, default: false
-  end
-end
