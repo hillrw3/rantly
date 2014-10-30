6d7a931153af98190037169e3f3715230d514300
Rob Hill <robert.hill0122@gmail.com>
2014-10-30 12:30:31 -0600

    fixed production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => "secret-dusk-3140.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "mighty-peak-5299.herokuapp.com" }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
