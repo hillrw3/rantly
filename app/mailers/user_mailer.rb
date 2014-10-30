1866d10bf6d414497ea561c24abdda06496bee27
Rob Hill <robert.hill0122@gmail.com>
2014-10-30 09:16:18 -0600

    changed password for email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "admin@rantly.com"
 
-  def registration_email(user)
+  def confirmation_email(user)
     @user = user
     @url = 'http://example.com/login'
     mail(to: @user.email, subject: "Welcome to Rantly!")
